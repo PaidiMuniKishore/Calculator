95f0a62a3ac850eeb77f3f7f132657145844d761
PAIDITHALUPULA MUNIKISHORE <138359241+PaidiMuniKishore@users.noreply.github.com>
2024-12-05 20:00:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+function addToDisplay(char) {
+  display.value += char;
+}
+function clearDisplay() {
+  display.value = '';
+}
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = 'Error';
+  }
+}
